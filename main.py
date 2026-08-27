8f80278780e4cc64669ee858e033358266189959
abdulrehmanatif <abdulrehmanatif51@gmail.com>
2026-08-27 22:09:48 +0500

    Stage 0: setup server and supabase client

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from supabase import create_client
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Server running and connected to Supabase"}
